0ec75c781007969b7f03c1d23bd699e80606ebe5
Itanor Strapazzon <itanor@gmail.com>
2022-01-11 21:25:28 -0300

    slices

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,33 @@ fn main() {
     let mut mut_string = String::from("mutable hello");
     mutable_reference(&mut mut_string);
     println!("string mutated: {}", mut_string);
+
+    // slices...
+    let phrase = String::from("some phrase to be processed");
+    let first_word_of_phrase = first_word(&phrase);
+    println!("{}", first_word_of_phrase);
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let _word = first_word_slice(&my_string[0..6]);
+    let _word = first_word_slice(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let _word = first_word_slice(&my_string);
+
+    // a string literal is a slice
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let _word = first_word_slice(&my_string_literal[0..6]);
+    let _word = first_word_slice(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let _word = first_word_slice(my_string_literal);
+    
+    integer_slice();
 }
 
 fn takes_ownership(some_string: String) {
@@ -39,5 +66,43 @@ fn calculate_len(s: &String) -> usize {
 
 fn mutable_reference(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+// slices (with *String type as param)
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+// slices (with string slices as param)
+fn first_word_slice(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn integer_slice() {
+    let a = [1,2,3,4,5];
+
+    let slice = &a[1..a.len()];
+    match slice.iter().nth(4) {
+        Some(val) => println!("{}", val),
+        None      => println!("without value"),
+    };
+
+    println!("{:?}", slice);
 }
 
